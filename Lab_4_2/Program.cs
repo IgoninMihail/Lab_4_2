6c8f4fb6d7529350ec1337ef13fa21a908beccd1
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-12-23 20:27:00 +0200

    Добавьте файлы проекта.

---
+++ Lab_4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] A = new int[5,5]
+            {
+                { 1,1,1,1,1},
+                { 0,1,1,1,0},
+                { 1,1,0,1,1},
+                { 0,1,1,1,0},
+                { 1,1,1,1,1}
+            };
+
+            int h = A.GetLength(0);
+            int w = A.GetLength(1);
+
+            for (int y = 0; y < h; y++)
+            {
+                for(int x = 0; x < w; x++)
+                {
+                    Console.Write(A[y,x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        } 
+    }
+}
